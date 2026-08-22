8a5b0fe248f5c913183b4e2d103c5714ef0c194d
Aaditiyaraj Chauhan <aaditiyarajchauhan.aadi@gmail.com>
2026-08-22 23:55:46 +0530

    Add number_divisible_by_5_and_3.c to check divisibility by AND and OR logical operators

---
+++ Conditions/multiple_condition/number_divisible_by_5_and_3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    if(n%3==0 && n%5==0){
+        printf("it is divisible by 5 and 3 both");
+    }
+    else {
+        printf("it is not divisible by 5 and 3 both");
+    }
+    return 0;
+}
