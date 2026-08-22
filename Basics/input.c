303bad22cf35a2f88899b851735940f003de6995
Aaditiyaraj Chauhan <aaditiyarajchauhan.aadi@gmail.com>
2026-08-22 23:23:51 +0530

    Add input.c demonstrating user input for two integers in C

---
+++ Basics/input.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter the number a:");
+    scanf("%d",&a);
+    printf("a=%d\n",a);
+    int b,c;
+    printf("Enter the number b and c:");
+    scanf("%d %d",&b,&c);
+    printf("b=%d\nc=%d",b,c);
+    return 0;
+}
