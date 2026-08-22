19dab5c02f8c312a9b8fe2a91d8ce4bcb24de71c
Aaditiyaraj Chauhan <aaditiyarajchauhan.aadi@gmail.com>
2026-08-23 00:01:45 +0530

    Add Side_of_triangle.c to check if three sides can form a triangle using Logical operators

---
+++ Conditions/multiple_condition/Side_of_triangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter first side of triangle:");
+    scanf("%d",&a);
+    printf("Enter second side of triangle:");
+    scanf("%d",&b);
+    printf("Enter third side of triangle:");
+    scanf("%d",&c);
+    if((a+b)>c && (b+c)>a && (c+a)>b){
+        printf("it is a triangle");
+    }
+    else{
+        printf("it is not a triangle");
+    }
+    return 0;
+}
