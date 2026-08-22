cfce3a9a3e597830b72d58d7ea9b48848d96a2e6
Aaditiyaraj Chauhan <aaditiyarajchauhan.aadi@gmail.com>
2026-08-22 23:23:17 +0530

    Add Variable.c demonstrating variable declaration and initialization

---
+++ Basics/Variable.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    a=10;
+    int b=4;
+    printf("the value of a is:");
+    printf("%d\n",a);
+    printf("the value of b is: %d\n",b);
+    int x;
+    x=7;
+    printf("%d",x);
+    x=9;
+    printf("\n");
+    printf("%d",x);
+    x=200;
+    printf("%d",x);
+    return 0;
+}
