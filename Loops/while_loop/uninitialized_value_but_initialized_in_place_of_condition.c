98831525efd1a68edb8644d7ecfbf735ce833299
Aaditiyaraj Chauhan <aaditiyarajchauhan.aadi@gmail.com>
2026-08-30 21:54:09 +0530

    Add uninitialized_value_but_initialized_in_place_of_condition.c to demonstrate infinite loop behavior in a while loop

---
+++ Loops/while_loop/uninitialized_value_but_initialized_in_place_of_condition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int i;
+    //we assign i value in place of condition
+    //in place of condition we assign value
+    while(i=10){  
+        printf("\n%d",i);
+        i=i+1;
+    }
+    return 0;
+}
+//this is infinite loop
