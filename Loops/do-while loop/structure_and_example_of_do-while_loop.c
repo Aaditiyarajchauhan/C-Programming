0e922ddc3c0e006fd8fc38e9add4c1740808c3f9
Aaditiyaraj Chauhan <aaditiyarajchauhan.aadi@gmail.com>
2026-08-30 21:54:18 +0530

    Add structure_and_example_of_do-while_loop.c to demonstrate the use of do-while loop in C

---
+++ Loops/do-while loop/structure_and_example_of_do-while_loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    /*
+    initialization;
+    do{
+        block;
+        updation;
+    }while(condition);
+    */
+    int i=10;
+    do{
+        printf("hello\n");
+        i++;
+    }while(i<10);
+    return 0;
+}
