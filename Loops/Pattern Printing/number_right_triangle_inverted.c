e23c27a329eacf85374498d7567ffc6edbdfe43c
Aaditiyaraj Chauhan <aaditiyarajchauhan.aadi@gmail.com>
2026-09-09 22:14:06 +0530

    Refactor number_right_triangle_inverted.c to improve pattern printing logic

--- Loops/Pattern Printing/number_right_triangle_inverted.c
+++ Loops/Pattern Printing/number_right_triangle_inverted.c
@@ -12,9 +12,19 @@ int main(){
     int n;
     printf("Enter the value of n:");
     scanf("%d",&n);
+    /*
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n+1-i;j++){
             printf("%d ",j);
+        }
+        printf("\n");
+    }
+    */
+    for(int i=1;i<=n;i++){
+        int a=1;
+        for(int j=n;j>=i;j--){
+            printf("%d ",a);
+            a=a+1;
         }
         printf("\n");
     }
